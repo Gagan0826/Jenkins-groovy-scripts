83f741bedc8936a90c7360fc09c57504758880a6
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-16 11:04:31 +0530

    Update build-infra.groovy

--- build-infra.groovy
+++ build-infra.groovy
@@ -1,11 +1,6 @@
 pipeline{
     agent any
     stages{
-        stage("github-repo-triggered"){
-            steps{
-                checkout scmGit(branches: [[name: '*/master']], browser: github(''), extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-github', url: 'https://github.com/Gagan0826/Terraform-infra.git']])
-            }
-        }
         stage("git clone"){
             steps{
             git credentialsId: 'jenkins-github', url: 'https://github.com/Gagan0826/Terraform-infra.git'
