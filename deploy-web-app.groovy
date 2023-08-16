a96d752fd2b56bfed8549fa4ed20cacd5364cc95
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-16 11:05:40 +0530

    Create deploy-web-app.groovy

---
+++ deploy-web-app.groovy
@@ -0,0 +1,38 @@
+pipeline{
+    agent any
+    stages{
+        stage("clone web-app from git"){
+            steps{
+                git branch: 'main', credentialsId: 'jenkins-github', url: 'https://github.com/Gagan0826/web-app.git'
+            }
+        }
+        stage("check who"){
+            steps{
+                sh "whoami"
+            }
+        }
+        stage("docker build"){
+            steps{
+                sh "docker build -t web-app-image ."
+            }
+        }
+        
+        stage("login to ecr"){
+            steps{
+                sh "aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 663839140840.dkr.ecr.ap-south-1.amazonaws.com"
+            }
+        }
+         
+        stage("tag image"){
+            steps{
+                sh "docker tag web-app-image:latest 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:latest"
+            }
+        }
+        stage("push to ecr"){
+            steps{
+                sh "docker push 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:latest"
+            }
+        }
+        
+    }
+}
