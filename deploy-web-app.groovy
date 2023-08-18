9c9353942aa8a03f28aef1073309aacbe451e937
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-18 14:28:49 +0530

    Update deploy-web-app.groovy

--- deploy-web-app.groovy
+++ deploy-web-app.groovy
@@ -11,11 +11,7 @@ pipeline{
                 git branch: 'main', credentialsId: 'jenkins-github', url: 'https://github.com/Gagan0826/web-app.git'
             }
         }
-        stage("check who"){
-            steps{
-                sh "whoami"
-            }
-        }
+
         stage("docker build"){
             steps{
                 sh "docker build -t web-app-image ."
@@ -30,14 +26,13 @@ pipeline{
          
         stage("tag image"){
             steps{
-                sh "docker tag web-app-image:latest 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:latest"
+                sh "docker tag web-app-image:latest 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:''v\$(expr \${BUILD_NUMBER} - 40)''"
             }
         }
         stage("push to ecr"){
             steps{
-                sh "docker push 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:latest"
+                sh "docker push 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:''v\$(expr \${BUILD_NUMBER} - 40)''"
             }
         }
-        
     }
 }
