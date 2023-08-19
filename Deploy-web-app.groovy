a965105e7c389f24f0a9d419bddf6df3649b65ab
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-19 17:11:35 +0530

    Create Deploy-web-app.groovy

---
+++ Deploy-web-app.groovy
@@ -0,0 +1,67 @@
+pipeline {
+    agent any
+    stages {
+        stage("Docker login") {
+            steps {
+                script {
+                    sshPublisher(
+                    publishers: [
+                        sshPublisherDesc(
+                            configName: 'ec2-rds',
+                            transfers: [
+                                sshTransfer(
+                                    execCommand: '''
+                                        aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 663839140840.dkr.ecr.ap-south-1.amazonaws.com
+                                        '''
+                                    )
+                                ]
+                            )
+                        ]   
+                    )   
+
+                }
+            }
+        }
+        stage("Pull Image From ECR") {
+            steps {
+                script {
+                    sshPublisher(
+                    publishers: [
+                        sshPublisherDesc(
+                            configName: 'ec2-rds',
+                            transfers: [
+                                sshTransfer(
+                                    execCommand: '''
+                                        docker pull 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:latest
+                                        '''
+                                    )
+                                ]
+                            )
+                        ]   
+                    )   
+
+                }
+            }
+        }
+        stage("Run the image") {
+            steps {
+                script {
+                    sshPublisher(
+                    publishers: [
+                        sshPublisherDesc(
+                            configName: 'ec2-rds',
+                            transfers: [
+                                sshTransfer(
+                                    execCommand: '''
+                                        docker run -d -p 80:80 663839140840.dkr.ecr.ap-south-1.amazonaws.com/python-web-app:v2
+                                        '''
+                                    )
+                                ]
+                            )
+                        ]   
+                    )   
+                }
+            }
+        }
+    }
+}
